3720948f6b9b19431e602e0d5f2414ea0cab248a
vrotondo <150374510+vrotondo@users.noreply.github.com>
2025-03-05 18:41:27 -0500

    Update gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,15 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// Function to check temperature and control watering
+function checkTemperature(temperature) {
+    if (temperature > 80) {
+        console.log("Watering on");
+    } else {
+        console.log("Watering off");
+    }
+}
+
+console.log(checkTemperature(temperature));
+
